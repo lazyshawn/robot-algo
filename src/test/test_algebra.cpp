a9638586fbd2e2961b94033c374157d47230a73e
lazyshawn <hitszshaohu@gmail.com>
2023-05-14 15:55:49 +0800

    reorgenized for CMakeLists.txt

---
+++ src/test/test_algebra.cpp
@@ -0,0 +1,8 @@
+#include "geometry/algebra.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+  Eigen::Vector3d target{1,2,3}, norm{1,1,1}, viaPoint{0,0,0};
+  std::cout << dist2plane(target, norm, viaPoint) << std::endl;
+  return 0;
+}
