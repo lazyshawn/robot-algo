d2b0ca602b95c9752cf49350a4fd9a1dd091edd9
lazyshawn <hitszshaohu@gmail.com>
2024-01-20 17:12:01 +0800

    perf: python/kinematics simplify parameters

--- script/user_interface/kinematics.py
+++ script/user_interface/kinematics.py
@@ -35,80 +35,102 @@ def axis_angle_to_rotation(axis, angle):
 @brief  描述机器人构型的基类
        1. 存储机器人关节信息，连杆碰撞体的点云信息
        1. 计算机器人正运动学，绘制机器人模型
-@todo  多 TCP 配置文件的支持
 '''
 class RobotBase:
-    jointAxis, jointLimit, linkPoint = [], [], []
-    tran0 = []
-    numLink, numJoint = 0, 0
+    configFile = ""
+    configDir = ""
+    # 关节轴参数
+    jointAxis, jointLimit = [], []
+    numJoint = 0
     jointCouple = False
-    m0 = np.eye(4)
+    m0 = []
+    # 连杆参数
+    numLink = 0
+    linkPoint, linkName, tran0 = [], [], []
 
-    def __init__(self, fname):
-        self.load_config(fname)
+    '''
+    @brief  机械臂配置参数初始化
+    @param  configDir     文件夹路径
+    @param  configFile    配置文件路径
+    '''
+    def __init__(self, configDir, configFile = "config.json"):
+        self.configDir = configDir
+        self.configFile =  configFile
+        self.load_config()
 
     '''
     @brief  读取机械臂配置文件
-    @param  fname    配置文件路径
     '''
-    def load_config(self, fname):
+    def load_config(self):
         # 打开配置文件
-        with open(fname) as file:
+        with open(self.configDir + self.configFile) as file:
             config = json.load(file)
-            self.numJoint = config["num_of_joint"]
-            self.numLink = len(config["collision_link"])
             self.jointCouple = config["j2_j3_coupling"]
 
             # 关节限位
             self.jointLimit.append(np.array(config["joint_limits"]["positions"]["uppers"]) * np.pi / 180)
             self.jointLimit.append(np.array(config["joint_limits"]["positions"]["lowers"]) * np.pi / 180)
 
-            # 零位的 TCP 位姿 (w,x,y,z)
-            qua = config["end-effector"]["quaternion"]
-            rotMat = Rotation.from_quat((qua[3], qua[2], qua[1], qua[0]))
-            self.m0[0:3, 0:3] = rotMat.as_matrix()
-            self.m0[0:3, 3] = config["end-effector"]["position"]
-
             # 关节轴
+            self.numJoint = config["num_of_joint"] if ("num_of_joint" in config) else len(config["PoE_joint"])
             for joint in config["PoE_joint"]:
                 pnt = np.array(joint["position"])
                 dir = np.array(joint["rotation_axis"])
                 dir = dir / np.linalg.norm(dir)
                 self.jointAxis.append(np.concatenate([-1*np.cross(dir, pnt), dir]))
-            #  print(self.jointAxis)
 
-            # 读取连杆初始位姿
-            collisionLink = config["collision_link"]
-            for collis in collisionLink:
-                qua = collis["quaternion"]
+            # 零位的 TCP 位姿 (w,x,y,z)
+            numTcp = len(config["end-effector"])
+            self.m0 = [np.eye(4) for i in range(numTcp)]
+            for i in range(numTcp):
+                tcp = config["end-effector"][i]
+                qua = tcp["quaternion"]
                 rotMat = Rotation.from_quat((qua[3], qua[2], qua[1], qua[0]))
-                tmpTran = np.eye(4)
-                tmpTran[0:3, 0:3] = rotMat.as_matrix()
-                tmpTran[0:3, 3] = collis["position"]
-                self.tran0.append(tmpTran)
+                self.m0[i][0:3, 0:3] = rotMat.as_matrix()
+                self.m0[i][0:3, 3] = tcp["position"]
 
     '''
     @brief  加载连杆模型的点云数据，仅支持 ply 格式
-    @param  robotDir    点云数据所在文件夹
-    @param  prefix      点云文件的命名前缀
-    @param  scale       点云单位缩放，确保点云单位和配置文件中的连杆偏移单位一致
+    @param  scale    点云单位缩放，确保点云单位和配置文件中的连杆偏移单位一致
     '''
-    def load_obj_model(self, robotDir, prefix = "link_", scale = 1):
-        for i in range(self.numLink):
-            fname = robotDir + prefix + str(i) + ".ply"
+    def load_obj_model(self, scale = 1):
+        with open(self.configDir + self.configFile) as file:
+            config = json.load(file)
+            # 连杆数
+            self.numLink = config["num_of_link"] if ("num_of_link" in config) else self.numJoint + 1
+            fileName = ["" for i in range(self.numLink)]
+            self.linkName = ["" for i in range(self.numLink)]
+
+            collisionLink = config["collision_link"]
+            for i in range(self.numLink):
+                # 读取连杆初始姿态
+                qua = collisionLink[i]["quaternion"]
+                rotMat = Rotation.from_quat((qua[3], qua[2], qua[1], qua[0]))
+                tmpTran = np.eye(4)
+                tmpTran[0:3, 0:3] = rotMat.as_matrix()
+                tmpTran[0:3, 3] = collisionLink[i]["position"]
+                self.tran0.append(tmpTran)
+                # 读取模型文件名称
+                fileName[i] = collisionLink[i]["model_name"]
+                # 连杆名称
+                self.linkName[i] = collisionLink[i]["name"]
+
+        for link in fileName:
+            fname = self.configDir + link
             data = np.loadtxt(fname, skiprows=8).transpose() * scale
             self.linkPoint.append(data)
 
     '''
     @brief  求解正运动学
-    @param  theta    各关节的关节角
-    @return trans    TCP 相对于基坐标系的位姿
+    @param  theta     各关节的关节角
+    @param  tcpIdx    TCP 索引
+    @return trans     TCP 相对于基坐标系的位姿
     '''
-    def solve_forward_kinematics(self, theta):
+    def solve_forward_kinematics(self, theta, tcpIdx = 0):
         # 2,3 关节耦合
         if(self.jointCouple):
             theta[2] = theta[2] + theta[1]
-        trans = self.m0
+        trans = self.m0[tcpIdx]
         for i in range(self.numJoint):
             idx = self.numJoint - i - 1
             trans = np.matmul(lieSE3(self.jointAxis[idx] * theta[idx]), trans)
@@ -116,39 +138,40 @@ class RobotBase:
 
     '''
     @brief  绘制机器人模型
+            需要先加载机器人连杆模型 `load_obj_model()`
     @param  ax          matplotlib 的坐标轴
     @param  theta       各关节的关节角
-    @param  coordLen    基坐标系和TCP坐标系的坐标轴长度
+    @param  coordLen    基坐标系和TCP坐标系的坐标轴长度，小于等于0时不绘制坐标系
+    @param  tcpIdx      TCP 索引
     '''
-    def plot_model(self, ax, theta, coordLen = 0):
+    def plot_model(self, ax, theta, coordLen = 0, tcpIdx = 0):
         handle = []
         # 机器人基座 - link_0
         point = self.linkPoint[0]
         point = np.matmul(self.tran0[0][0:3, 0:3], point) + self.tran0[0][0:3,3].reshape(3,1)
-        handle.append(ax.scatter(*[point[coord,:] for coord in range(3)], s=2, label = "Base_link"))
+        handle.append(ax.scatter(*[point[coord,:] for coord in range(3)], s=2, label = self.linkName[0]))
         # 计算每个连杆的旋转矩阵
-        tran = [np.eye(4) for i in range(self.numJoint)]
+        tran = [np.eye(4) for i in range(self.numLink)]
         # 第 i 个关节的运动
         for i in range(self.numJoint-1, -1, -1):
             # 第 j 个连杆
-            for j in range(self.numJoint-1, i-1, -1):
+            for j in range(self.numLink-1, i-1, -1):
                 tran[j] = np.matmul(lieSE3(self.jointAxis[i] * theta[i]), tran[j])
         # 机器人连杆 - link_i
-        for i in range(self.numJoint):
+        for i in range(self.numLink-1):
             # 初始位置
             point = self.linkPoint[i+1]
             # 影响当前连杆的转换矩阵
             tmpTrans = np.matmul(tran[i], self.tran0[i+1])
             # 当前连杆的位置
             point = np.matmul(tmpTrans[0:3, 0:3], point) + tmpTrans[0:3,3].reshape(3,1)
-            handle.append(ax.scatter(*[point[coord,:] for coord in range(3)], s=2, label="link_"+str(i+1)))
-            #  ax.scatter(*[point[coord,:] for coord in range(3)], s=2)
+            handle.append(ax.scatter(*[point[coord,:] for coord in range(3)], s=2, label=self.linkName[i+1]))
 
         if (coordLen > 0):
             # 基坐标系位姿
             plotUtils.plotCoordinate(ax, self.tran0[0][0:3,0:3], coordLen, self.tran0[0][0:3,3])
             # TCP 位姿
-            tmpTrans = np.matmul(tran[self.numJoint-1], self.m0)
+            tmpTrans = np.matmul(tran[self.numJoint-1], self.m0[tcpIdx])
             plotUtils.plotCoordinate(ax, tmpTrans[0:3,0:3], coordLen, tmpTrans[0:3,3])
 
         plotUtils.set_ax_equal(ax)
@@ -163,8 +186,8 @@ if __name__ == "__main__":
     fig = plt.figure('plotUtils test',figsize=(8,6))
     ax =plt.subplot(projection='3d')
 
-    robot = RobotBase("data/r2700/config.json")
-    robot.load_obj_model("data/r2700/", "link_", 1000)
+    robot = RobotBase("data/r2700/")
+    robot.load_obj_model(1000)
 
     robot.plot_model(ax, theta, 1000)
     print(robot.solve_forward_kinematics(theta))
--- script/visualization/plot_robot.py
+++ script/visualization/plot_robot.py
@@ -6,15 +6,12 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     fig = plt.figure('plotUtils test',figsize=(8,6))
     ax =plt.subplot(projection='3d')
-    # 系统默认的颜色顺序. Ref: https://stackoverflow.com/questions/42086276
-    #  colorMap = [p['color'] for p in plt.rcParams['axes.prop_cycle']]
-    colorMap = plt.get_cmap("tab10")
 
-    theta = np.array([90,45,0,0,0,0]) * np.pi / 180
+    theta = np.array([0,-45,45,0,90,0]) * np.pi / 180
 
-    robot = kinematics.RobotBase("data/cloos/config.json")
-    robot.load_obj_model("data/cloos/", "J")
-    print(robot.solve_forward_kinematics(theta))
+    robot = kinematics.RobotBase("data/cloos/")
+    robot.load_obj_model()
+    #  print(robot.solve_forward_kinematics(theta))
 
     robot.plot_model(ax, theta)
 
